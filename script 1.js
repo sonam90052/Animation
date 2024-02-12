e7cbb735c6d8786d13f256915c613be4ec4a5a2b
sonam90052 <150127175+sonam90052@users.noreply.github.com>
2024-02-12 14:41:08 +0530

    Add files via upload

---
+++ script 1.js
@@ -0,0 +1,40 @@
+Variable=93
+a=22%7
+b=26+74
+c=52-34
+d=42*5
+e=32/4  
+document.write(Variable,"<br>")
+ document.write(a,"<br>",b,"<br>",c,"<br>",d,"<br>",e,"<br>")
+
+//  if(age>=18){
+// document.write("Eligible for vote","<br>")
+//  }
+//  else{
+// document.write("Not eligible for vote","<br>")
+//  }
+
+//  number=prompt("enter your number")
+//  if(number%2==0){
+// document.write("even number")
+//  }
+//  else{
+// document.write("odd number")
+//  }
+ value=prompt("enter your value")
+ if(value>0){
+document.write("Positive value")
+ }
+ else{
+
+document.write("negative value")
+ }
+ marks1=prompt("enter your marks1")
+ marks2=prompt("enter your marks2")
+ if(marks1+marks2>=100){
+document.write("Pass")
+
+ }
+ else{
+document.write("Fail")
+ }
